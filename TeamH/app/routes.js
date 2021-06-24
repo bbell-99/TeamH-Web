bb906f7116171aee6671d311fb92960023c61467
Matthew Doherty <matthew.doherty1@kainos.com>
2021-06-24 13:29:55 +0100

    Use case 1 validation

--- TeamH/app/routes.js
+++ TeamH/app/routes.js
@@ -15,8 +15,21 @@ router.get('/newemp', async (req, res) => {
 router.post('/newemp', async (req, res) => {
 	const emp = req.body
 	console.log(emp)
-	if (!emp) {
-		console.log('emp empty')
+	if (emp.nin.search(/^\s*[a-zA-Z]{2}(?:\s*\d\s*){6}[a-zA-Z]?\s*$/)) {
+		res.locals.errormessage =
+			'NIN must begin with 2 letters and be no longer than 9 characters'
+		res.render('newemp', req.body)
+		console.log('nin error')
+	} else if (emp.ban.search(/^[0-9]{8}$/)) {
+		res.locals.errormessage = 'BAN must be only numbers and 8 numbers long'
+		res.render('newemp', req.body)
+	} else if (emp.sortcode.search(/^[0-9]{6}$/)) {
+		res.locals.errormessage = 'Sortcode must be only numbers and 6 numbers long'
+		res.render('newemp', req.body)
+	} else if (emp.salary.search(/^[0-9]\d{4,}$/)) {
+		res.locals.errormessage =
+			'Salary must be only numbers and atleast 5 numbers long'
+		res.render('newemp', req.body)
 	} else {
 		let insertedKey = await dbConn.newEmp(emp)
 		res.render('emps', { emps: await dbConn.getEmps() })
