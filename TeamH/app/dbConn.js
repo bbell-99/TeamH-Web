83983f9ed04dcdf36ff715a5d6a67e19ef5ed23f
Gavin Bell <gavin.bell@kainos.com>
2021-06-24 14:39:15 +0100

    Use case 3

--- TeamH/app/dbConn.js
+++ TeamH/app/dbConn.js
@@ -4,43 +4,48 @@ const util = require('util')
 const db = wrapDB(dbconfig)
 
 function wrapDB(dbconfig) {
-	const pool = mysql.createPool(dbconfig)
-	return {
-		query(sql, args) {
-			console.log('in query in wrapper')
-			return util.promisify(pool.query).call(pool, sql, args)
-		},
-		release() {
-			return util.promisify(pool.releaseConnection).call(pool)
-		},
-	}
+    const pool = mysql.createPool(dbconfig)
+    return {
+        query(sql, args) {
+            console.log('in query in wrapper')
+            return util.promisify(pool.query).call(pool, sql, args)
+        },
+        release() {
+            return util.promisify(pool.releaseConnection).call(pool)
+        },
+    }
 }
 
-exports.getEmps = async () => {
-	const data = await db.query(
-		'SELECT emp_id AS `ID`, emp_name AS `Name` FROM Employee'
-	)
-	console.log(data)
-	return data
+exports.getEmps = async() => {
+    const data = await db.query(
+        'SELECT emp_id AS `ID`, emp_name AS `Name` FROM Employee'
+    )
+    console.log(data)
+    return data
 }
 
-exports.getSalesEmps = async () => {
-	const data = await db.query(
-		'SELECT s.emp_id AS `ID`, e.emp_name AS `Name`, e.address AS `Address`, e.nin as `NIN`, e.ban AS `BAN`, e.sortcode AS `Sortcode`, e.Salary AS `Salary`, s.commission_rate as `CommissionRate`, s.total_sales_value AS `TotalSalesValues` FROM Sales_Employee s JOIN Employee e WHERE s.emp_id = e.emp_id'
-	)
-	console.log(data)
-	return data
+exports.getSalesEmps = async() => {
+    const data = await db.query(
+        'SELECT s.emp_id AS `ID`, e.emp_name AS `Name`, e.address AS `Address`, e.nin as `NIN`, e.ban AS `BAN`, e.sortcode AS `Sortcode`, e.Salary AS `Salary`, s.commission_rate as `CommissionRate`, s.total_sales_value AS `TotalSalesValues` FROM Sales_Employee s JOIN Employee e WHERE s.emp_id = e.emp_id'
+    )
+    console.log(data)
+    return data
 }
 
-exports.getTechEmps = async () => {
-	const data = await db.query(
-		'SELECT t.emp_id AS `ID`, e.emp_name AS `Name`, e.address AS `Address`, e.nin as `NIN`, e.ban AS `BAN`, e.sortcode AS `Sortcode`, e.Salary AS `Salary`, t.cv, t.photo FROM Technical_Employee t JOIN Employee e WHERE t.emp_id = e.emp_id'
-	)
-	console.log(data)
-	return data
+exports.getTechEmps = async() => {
+    const data = await db.query(
+        'SELECT t.emp_id AS `ID`, e.emp_name AS `Name`, e.address AS `Address`, e.nin as `NIN`, e.ban AS `BAN`, e.sortcode AS `Sortcode`, e.Salary AS `Salary`, t.cv, t.photo FROM Technical_Employee t JOIN Employee e WHERE t.emp_id = e.emp_id'
+    )
+    console.log(data)
+    return data
 }
 
-exports.newEmp = async (newEmp) => {
-	let results = await db.query('INSERT INTO Employee SET ?', newEmp)
-	return results.insertId
+exports.newEmp = async(newEmp) => {
+    let results = await db.query('INSERT INTO Employee SET ?', newEmp)
+    return results.insertId
+}
+
+exports.newSalesEmp = async(newEmp) => {
+    let results = await db.query('INSERT INTO Sales_Employee SET ?', newEmp)
+    return results.insertId
 }
--- TeamH/app/routes.js
+++ TeamH/app/routes.js
@@ -4,43 +4,63 @@ const dbConn = require('./dbConn')
 
 // Add your routes here - above the module.exports line
 
-router.get('/emps', async (req, res) => {
-	res.render('emps', { emps: await dbConn.getEmps() })
+router.get('/emps', async(req, res) => {
+    res.render('emps', { emps: await dbConn.getEmps() })
 })
 
-router.get('/newemp', async (req, res) => {
-	res.render('newemp')
+router.get('/newemp', async(req, res) => {
+    res.render('newemp')
 })
 
-router.get('/empdepartment', async (req, res) => {
-	res.render('empdepartment', {
-		salesemps: await dbConn.getSalesEmps(),
-		techemps: await dbConn.getTechEmps(),
-	})
+router.get('/empdepartment', async(req, res) => {
+    res.render('empdepartment', {
+        salesemps: await dbConn.getSalesEmps(),
+        techemps: await dbConn.getTechEmps(),
+    })
 })
 
-router.post('/newemp', async (req, res) => {
-	const emp = req.body
-	console.log(emp)
-	if (emp.nin.search(/^\s*[a-zA-Z]{2}(?:\s*\d\s*){6}[a-zA-Z]?\s*$/)) {
-		res.locals.errormessage =
-			'NIN must begin with 2 letters and be no longer than 9 characters'
-		res.render('newemp', req.body)
-		console.log('nin error')
-	} else if (emp.ban.search(/^[0-9]{8}$/)) {
-		res.locals.errormessage = 'BAN must be only numbers and 8 numbers long'
-		res.render('newemp', req.body)
-	} else if (emp.sortcode.search(/^[0-9]{6}$/)) {
-		res.locals.errormessage = 'Sortcode must be only numbers and 6 numbers long'
-		res.render('newemp', req.body)
-	} else if (emp.salary.search(/^[0-9]\d{4,}$/)) {
-		res.locals.errormessage =
-			'Salary must be only numbers and atleast 5 numbers long'
-		res.render('newemp', req.body)
-	} else {
-		let insertedKey = await dbConn.newEmp(emp)
-		res.render('emps', { emps: await dbConn.getEmps() })
-	}
+router.post('/newemp', async(req, res) => {
+    const emp = req.body
+    console.log(emp)
+    if (emp.nin.search(/^\s*[a-zA-Z]{2}(?:\s*\d\s*){6}[a-zA-Z]?\s*$/)) {
+        res.locals.errormessage =
+            'NIN must begin with 2 letters and be no longer than 9 characters'
+        res.render('newemp', req.body)
+        console.log('nin error')
+    } else if (emp.ban.search(/^[0-9]{8}$/)) {
+        res.locals.errormessage = 'BAN must be only numbers and 8 numbers long'
+        res.render('newemp', req.body)
+    } else if (emp.sortcode.search(/^[0-9]{6}$/)) {
+        res.locals.errormessage = 'Sortcode must be only numbers and 6 numbers long'
+        res.render('newemp', req.body)
+    } else if (emp.salary.search(/^[0-9]\d{4,}$/)) {
+        res.locals.errormessage =
+            'Salary must be only numbers and atleast 5 numbers long'
+        res.render('newemp', req.body)
+    } else {
+        let insertedKey = await dbConn.newEmp(emp)
+        res.render('emps', { emps: await dbConn.getEmps() })
+    }
 })
+
+
+router.post("/newsalesemp", async(req, res) => {
+    const emp = req.body;
+    console.log(emp);
+    if (emp.commission_rate.search(/^\d*\.?\d*$/)) {
+        res.render("newsalesemp", { emps: await dbConn.getEmps(), errormessage: "Commission rate must be numerical" });
+    } else if (emp.total_sales_value.search(/^\d*\.?\d*$/)) {
+        res.render("newsalesemp", { emps: await dbConn.getEmps(), errormessage: "Total sales must be numerical" });
+    } else {
+        let insertedKey = await dbConn.newSalesEmp(emp);
+        res.render("newsalesemp", { emps: await dbConn.getEmps(), salesempadded: "Sales employee added" });
+    }
+});
+
+router.get("/newsalesemp", async(req, res) => {
+    const emp = req.body;
+    console.log(emp);
+    res.render("newsalesemp", { emps: await dbConn.getEmps() });
+});
 
 module.exports = router
